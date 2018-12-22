8a21aeaa3593a32f4190eb873e1834fc84df1915
ngoni mugandani <ngonimug@gmail.com>
2018-12-22 22:07:13 +0200

    migrations plus db connections

--- dream_team/dream_team/settings.py
+++ dream_team/dream_team/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corporate',
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'dream_team.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dreamteam_db',
+        'USER': 'testing',
+        'PASSWORD': 'testing',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
